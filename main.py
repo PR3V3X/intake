4f624e097bd8d20f9e87d0b0c54848488e49a04f
Paul Lovins <59129974+PR3V3X@users.noreply.github.com>
2021-10-04 09:56:48 -0500

    Main App

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 def main():
-    print("_" * 78)
-    print("|~~~>                       Welcome to Intake 0.1!                       <~~~|")
-    print("|~~~~~~>         The Simple Python Repair Intake Application          <~~~~~~|")
-    print("|~~~~~~~~~>                                                        <~~~~~~~~~|")
-    print("|~~~~~~~~~~~>                     Enjoy! :)                     <~~~~~~~~~~~~|")
-    print("|____________________________________________________________________________|")
+	print("_" * 78)
+	print("|~~~>                       Welcome to Intake 0.1!                       <~~~|")
+	print("|~~~~~~>         The Simple Python Repair Intake Application          <~~~~~~|")
+	print("|~~~~~~~~~>                                                        <~~~~~~~~~|")
+	print("|~~~~~~~~~~~>                     Enjoy! :)                     <~~~~~~~~~~~~|")
+	print("|____________________________________________________________________________|")
 
-    # Menu
-    print("|_____________________________> Intake Home <________________________________|")
-    print("| 1.) Start Device Inspection                2.) View Old Device Inspections |")
+	# Menu
+	print("|_____________________________> Intake Home <________________________________|")
+	print("| 1.) Start Device Inspection                2.) View Old Device Inspections |")
+	print("|____________________________________________________________________________|")
+	print("| ")
 
-    choice = input("| Make selction using numbers, then press enter: ")
-    print("|____________________________________________________________________________|")    
+	select = input("| Make selction using numbers, then press enter: ")
+	print("|____________________________________________________________________________|")    
  
 
-# Choice Selection
+	# Selection
+	if select == "1":
+		intake_start()
+	elif select == "2":
+		intake_old()
+	else:
+		print("Please choose a valid option...")
+		print("_" * 78)
+		main()
+
+main()
