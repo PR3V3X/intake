5e8de55dbf871033b48de74dcfeff177a2fc72c0
Paul Lovins <59129974+PR3V3X@users.noreply.github.com>
2021-10-04 09:42:24 -0500

    Create main.py
    Main Application

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    print("_" * 78)
+    print("|~~~>                       Welcome to Intake 0.1!                       <~~~|")
+    print("|~~~~~~>         The Simple Python Repair Intake Application          <~~~~~~|")
+    print("|~~~~~~~~~>                                                        <~~~~~~~~~|")
+    print("|~~~~~~~~~~~>                     Enjoy! :)                     <~~~~~~~~~~~~|")
+    print("|____________________________________________________________________________|")
+
+    # Menu
+    print("|_____________________________> Intake Home <________________________________|")
+    print("| 1.) Start new intake. 2.) View Old intakes                                 |")
+    print("| 2.) View old intake")
